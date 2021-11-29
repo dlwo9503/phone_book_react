c1b77a09bf769aa0f4fc1ad6be9279f6892f03fe
jaes <dlwo9503@naver.com>
2021-11-29 13:03:07 +0900

    CSS 수정

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,35 +1,48 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
-const Item = styled.li`
+const Items = styled.li`
     display: flex;
-    background-color: ${(props) => (props.id ? "rgb(40, 173, 250)" : "inherit")}
+    background-color: ${(props) => (props.id ? "rgb(40, 173, 250)" : "inherit")};
 `;
 
-const ListItem = ({ contact, id, name, phone_number, setSelectContactId, selectContactId }) => {
-    const arr = Array.from({length: contact.length}, () => false);
-    const [arr2, setArr2] = useState(arr);
-    console.log('arr' + arr);
-    
+const Item = styled.button`
+    flex: 1 1 0%;
+    height: 80px;
+    border-width: 0px 0px 1px;
+    border-top-style: initial;
+    border-right-style: initial;
+    border-left-style: initial;
+    border-top-color: initial;
+    border-right-color: initial;
+    border-left-color: initial;
+    border-image: initial;
+    border-bottom-style: solid;
+    border-bottom-color: rgb(212, 211, 211);
+    display: flex;
+    flex-direction: column;
+    -webkit-box-pack: center;
+    justify-content: center;
+    padding-left: 20px;
+    z-index: 0;
+    background-color: transparent;
+    color: ${(props) => (props.id ? "white" : "inherit")};
+    cursor: pointer;
+`;
+
+const ListItem = ({ id, name, phone_number, setSelectContactId, selectContactId }) => {
     
     const handleListItem = () => {
-        console.log(id);
         setSelectContactId(id);
-        
-        let newArray = arr2;
-        setArr2( arr );
-        newArray[id - 1] = true;
-        setArr2( newArray );
-        console.log('2', arr2);
     }
 
     return (
-        <Item id={arr2[id - 1]} onClick={handleListItem}>
-            <button className="hPsvfS" id={id}>
+        <Items id={id === selectContactId} onClick={handleListItem}>
+            <Item id={id === selectContactId} >
                 <div className="jcTaHb">{name}</div>
                 <div className="hydYaP">{phone_number}</div>
-            </button>
-        </Item>
+            </Item>
+        </Items>
     );
 };
 
