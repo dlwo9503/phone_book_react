899946f051dd72de98bd57f3b9c0e3d3d0a53ad2
이재성 <dlwo9503@naver.com>
2021-12-02 13:27:43 +0900

    .

--- src/components/List.js
+++ src/components/List.js
@@ -6,7 +6,7 @@ const List = ({ keyword, contact, setSelectContactId, selectContactId }) => {
         <ul className="eysHZq">
             {
                 contact && contact
-                    .filter(item => item.name.indexOf(keyword) !== -1 || item.phoneNumber.indexOf(keyword) !== -1)
+                    .filter(item => item.name.indexOf(keyword) !== -1 || toString(item.age).indexOf(keyword) !== -1 || item.phoneNumber.indexOf(keyword) !== -1 || item.email.indexOf(keyword) !== -1 || item.description.indexOf(keyword) !== -1)
                     .map(item => <ListItem id={ item.id } name={ item.name } phoneNumber={ item.phoneNumber } setSelectContactId={ setSelectContactId } selectContactId = {selectContactId}/>)
             }
         </ul>
